19bbece5d0ae72e662c4c650132c5ae82e54688e
restaurantjob714-LT <restaurantjob714@gmail.com>
2026-04-20 17:16:31 -0700

    change airbnb Edit button background

--- app/page.tsx
+++ app/page.tsx
@@ -1082,7 +1082,7 @@ return (
                   <div className="flex flex-col sm:flex-row gap-2">
                   <button
                     
-                    className="bg-gray-100 hover:bg-gray-200 text-gray-800 px-4 py-2 rounded-xl font-medium transition"
+                    className="bg-light green-100 hover:bg-light green-200 text-white-800 px-4 py-2 rounded-xl font-medium transition"
                     onClick={() => startEditing(p)}
                   >
                     Edit
